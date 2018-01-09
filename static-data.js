5a4694e1f2097658739b4ceb2a7a50792faf06bd
Stefan Englmeier <stef.engl.se@gmail.com>
2018-01-09 22:20:02 +0100

    Initial Commit

---
+++ static-data.js
@@ -0,0 +1,14 @@
+const TINY_API_KEY = 'yuVX20TE7r1ebwVjNTtQTaV469cGJmcW';
+
+const IMAGE_SOURCE_PATH = '';
+
+const RESIZING_INFO = {
+    x: '',
+    y: ''
+}
+
+export {
+    TINY_API_KEY,
+    IMAGE_SOURCE_PATH,
+    RESIZING_INFO
+}
